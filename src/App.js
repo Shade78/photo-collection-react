1e7b55036ca3ed5346d62e8bb1d3ec94750c5203
Shade78 <shademoon322@gmail.com>
2024-08-19 00:38:46 +0300

    Init
    render after mockapi request and styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Collection } from "./Collection";
+import "./index.scss";
 
 function App() {
+  const [collections, setCollections] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://66c25831f83fffcb587bbc9a.mockapi.io/photo_collections")
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        setCollections(json);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Ошибка при получении данных");
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Моя коллекция фотографий</h1>
+      <div className="top">
+        <ul className="tags">
+          <li className="active">Все</li>
+          <li>Горы</li>
+          <li>Море</li>
+          <li>Архитектура</li>
+          <li>Города</li>
+        </ul>
+        <input className="search-input" placeholder="Поиск по названию" />
+      </div>
+      <div className="content">
+        {collections.map((obj, index) => {
+          return <Collection key={index} name={obj.name} images={obj.photos} />;
+        })}
+      </div>
+      <ul className="pagination">
+        <li>1</li>
+        <li className="active">2</li>
+        <li>3</li>
+      </ul>
     </div>
   );
 }
