3d004c12fb52ccff60ef0cdc5ebbd9b6385bef2f
Shade78 <shademoon322@gmail.com>
2024-08-20 00:07:47 +0300

    ready
    added filter by category, search collection, pagination by mockapi request

--- src/App.js
+++ src/App.js
@@ -2,44 +2,90 @@ import React from "react";
 import { Collection } from "./Collection";
 import "./index.scss";
 
+const cats = [
+  { name: "Все" },
+  { name: "Море" },
+  { name: "Горы" },
+  { name: "Архитектура" },
+  { name: "Города" },
+];
+
 function App() {
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [page, setPage] = React.useState(1);
+  const [categoryId, setCategoryId] = React.useState(0); // 0 - все  категории
+  const [searchValue, setSearchValue] = React.useState("");
   const [collections, setCollections] = React.useState([]);
 
   React.useEffect(() => {
-    fetch("https://66c25831f83fffcb587bbc9a.mockapi.io/photo_collections")
+    setIsLoading(true);
+
+    const category = categoryId ? `category=${categoryId}` : "";
+
+    fetch(
+      `https://66c25831f83fffcb587bbc9a.mockapi.io/photo_collections?page=${page}&limit=2&${category}`
+    )
       .then((res) => res.json())
       .then((json) => {
-        console.log(json);
         setCollections(json);
       })
       .catch((err) => {
         console.warn(err);
         alert("Ошибка при получении данных");
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
-  }, []);
+  }, [categoryId, page]);
 
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
       <div className="top">
         <ul className="tags">
-          <li className="active">Все</li>
-          <li>Горы</li>
-          <li>Море</li>
-          <li>Архитектура</li>
-          <li>Города</li>
+          {cats.map((obj, i) => (
+            <li
+              onClick={() => setCategoryId(i)}
+              className={categoryId === i ? "active" : ""}
+              key={obj.name}
+            >
+              {obj.name}
+            </li>
+          ))}
         </ul>
-        <input className="search-input" placeholder="Поиск по названию" />
+        <input
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          className="search-input"
+          placeholder="Поиск по названию"
+        />
       </div>
       <div className="content">
-        {collections.map((obj, index) => {
-          return <Collection key={index} name={obj.name} images={obj.photos} />;
-        })}
+        {isLoading ? (
+          <h2>Идет загрузка...</h2>
+        ) : (
+          collections
+            .filter((obj) =>
+              obj.name.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((obj, index) => {
+              return (
+                <Collection key={index} name={obj.name} images={obj.photos} />
+              );
+            })
+        )}
       </div>
       <ul className="pagination">
-        <li>1</li>
-        <li className="active">2</li>
-        <li>3</li>
+        {/* mockapi не может сказать сколько осталось страниц, поэтому будто имеем 5 страниц */}
+        {[...Array(5)].map((_, i) => (
+          <li
+            onClick={() => setPage(i + 1)}
+            className={page === i + 1 ? "active" : ""}
+            key={i}
+          >
+            {i + 1}
+          </li>
+        ))}
       </ul>
     </div>
   );
